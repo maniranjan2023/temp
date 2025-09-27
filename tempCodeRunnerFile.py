4d5d7a4cab42ff2053c433517035c1927f4fb2f1
mani2578542 <maniranjan@revaluate180.com>
2025-09-27 16:21:56 +0530

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,56 @@
+
+from flask import Flask, render_template, request
+import os
+import pickle
+import numpy as np
+
+def load_pickle_file(filename):
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"Required file '{filename}' not found in {os.path.dirname(__file__)}")
+    with open(filepath, 'rb') as f:
+        return pickle.load(f)
+
+model = load_pickle_file('model.pkl')
+sc = load_pickle_file('standscaler.pkl')
+mx = load_pickle_file('minmaxscaler.pkl')
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    N = request.form['Nitrogen']
+    P = request.form['Phosporus']
+    K = request.form['Potassium']
+    temp = request.form['Temperature']
+    humidity = request.form['Humidity']
+    ph = request.form['pH']
+    rainfall = request.form['Rainfall']
+
+    feature_list = [N, P, K, temp, humidity, ph, rainfall]
+    single_pred = np.array(feature_list).reshape(1, -1)
+
+    mx_features = mx.transform(single_pred)
+    sc_mx_features = sc.transform(mx_features)
+    prediction = model.predict(sc_mx_features)
+
+    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+
+    if prediction[0] in crop_dict:
+        crop = crop_dict[prediction[0]]
+        result = "{} is the best crop to be cultivated right there".format(crop)
+    else:
+        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
+    return render_template('index.html',result = result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
